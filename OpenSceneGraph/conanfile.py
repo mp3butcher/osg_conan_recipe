5acef6b05f5baa4755270e8e58eb5bcf0a974a6b
Julien Valentin <mp3butcher@hotmail.com>
2018-01-15 00:27:50 +0100

    few improvements

--- OpenSceneGraph/conanfile.py
+++ OpenSceneGraph/conanfile.py
@@ -3,7 +3,7 @@ import os
 import pip
 import subprocess
 
-
+from conans.tools import os_info, SystemPackageTool
 
 class OpenSceneGraphConan(ConanFile):
     name = "OpenSceneGraph"
@@ -13,35 +13,67 @@ class OpenSceneGraphConan(ConanFile):
     description = "The OpenSceneGraph is an open source high performance 3D graphics toolkit, used by application developers in fields such as visual simulation, games, virtual reality, scientific visualization and modelling. Written entirely in Standard C++ and OpenGL it runs on all Windows platforms, OSX, GNU/Linux, IRIX, Solaris, HP-Ux, AIX and FreeBSD operating systems. The OpenSceneGraph is now well established as the world leading scene graph technology, used widely in the vis-sim, space, scientific, oil-gas, games and virtual reality industries."
     settings = "os", "compiler", "build_type", "arch"
     #do all options had to be wrapped to cmake?
-    options = {"shared": [True, False],"dev_deploy": [True, False]}
-    default_options = "shared=True","dev_deploy=True"
+    options = {"shared": [True, False], "dev_deploy": [True, False]}
+    default_options = "shared=True", "dev_deploy=True"
     generators = "cmake"
     copy_source_to_build_dir = False
     build_policy = "missing" #"always" #
     short_paths = True #for win<10 naming
+    
+    #Manually-specified variables were not used by the project:
+    #CONAN_COMPILER
+    #CONAN_COMPILER_VERSION
+    #CONAN_CXX_FLAGS
+    #CONAN_C_FLAGS
+    #CONAN_EXPORTED
+    #CONAN_LIBCXX
+    #CONAN_SHARED_LINKER_FLAGS
 
-    def source(self):
-        self.run("git clone https://github.com/openscenegraph/OpenSceneGraph.git")
+    def system_requirements(self):
+        self.output.warn("system_requirements: ")
+        pack_name = None
+        if os_info.linux_distro == "ubuntu":
+            self.run('sudo apt-get build-dep openscenegraph', True)
+            #gstreamer seams missing after build-dep
+            pack_name="libgstreamer1.0-dev libgstreamer1.0-dev"
+        elif os_info.linux_distro == "fedora" or os_info.linux_distro == "centos":
+            pack_name = "TODOpackage_names_in_fedora_and_centos"
+        elif os_info.is_macos:
+            pack_name = "TODOpackage_names_in_macos"
+        elif os_info.is_freebsd:
+            pack_name = "TODOpackage_names_in_freebsd"
+        elif os_info.is_solaris:
+            pack_name = "TODOpackage_names_in_solaris"
+
+        if pack_name:
+            installer = SystemPackageTool()
+            installer.install(pack_name) # Install the package, will update the package database if pack_name isn't already installed
+
+
+    def source(self):       
+	#win32 depend stuff ( perhaps should be put in system_requirements but i doubt there's a working dir in this hook)
         self.customDependenciesDownload()
+
+        #retrieve OSG 3.4 (seams 3.4.2)	
+        self.run("git clone https://github.com/openscenegraph/OpenSceneGraph.git")        
         self.run("cd OpenSceneGraph && git checkout origin/OpenSceneGraph-3.4")
         
-        # && git checkout static_shared")
         # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
         # if the packaged project doesn't have variables to s   et it properly
 
-       # tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)", '''PROJECT(MyHello)
-#include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-#conan_basic_setup()''')
+        # tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)", '''PROJECT(MyHello)
+        #include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+        #conan_basic_setup()''')
 
         
     def build(self):
-        
+        #Could NOT find GStreamer 
         cmake = CMake(self)
         cmake.configure(source_dir="%s/OpenSceneGraph" % self.source_folder)
         cmake.build()
 
         # Explicit way:
-        # self.run('cmake %s/hello %s' % (self.source_folder, cmake.command_line))
+        # self.run('cmake %s/OpenSceneGraph %s' % (self.source_folder, cmake.command_line))
         # self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
@@ -80,6 +112,10 @@ class OpenSceneGraphConan(ConanFile):
         
     def package_info(self):
         self.cpp_info.libs = ["osg"]
+        self.env_info.OSG_ROOT = self.package_folder
+        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
+        if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":     
+            self.env_info.PATH.append(os.path.join(self.package_folder, "3rdParty/bin"))
         
     def customDependenciesDownload(self):
         if  self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
@@ -97,7 +133,8 @@ class OpenSceneGraphConan(ConanFile):
                 elif self.settings.compiler.version == 12: url=serv+"3rdParty_VS2013.5_v120_x64_V10_full.7z"
                 elif self.settings.compiler.version == 14: url=serv+"3rdParty_VS2017_v141_x64_V11_full.7z"
 
-            if url == "": raise Exception("Binary does not exist for these settings edit customDependenciesDownload() in OSG conan files in order to set 3rd Party url")   
+            if url == "": 
+                raise Exception("Binary does not exist for these settings edit customDependenciesDownload() in OSG conan files in order to set 3rd Party url")   
             
             self.output.warn('retrieve built dependency at: '+serv)
             self.output.warn("Downloading: %s" % url)
